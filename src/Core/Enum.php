50b3e1019fa36a50ecc342b99769724224ca2e51
Wasseem Khayrattee <w@7php.com>
2021-11-12 17:01:50 +0400

    feat: add more enums

--- src/Core/Enum.php
+++ src/Core/Enum.php
@@ -1,8 +1,16 @@
 <?php
-
+/**
+ *  A fake Enumeration class to keep things centralised
+ *
+ * @author Wasseem Khayrattee <hey@wk.contact>
+ * @github wkhayrattee
+ */
 namespace WKWPB2;
 
 class Enum
 {
     const PLUGIN_KEY = 'WK_WP_B2';
+    //Log files
+    const LOG_FILE_MESSAGE = 'wkwpb2_plugin.log';
+    const LOG_FILE_ERROR = 'wkwpb2_plugin_error.log';
 }
