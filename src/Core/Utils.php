43fdc81dd547f304fc549beb1257f255ef2fb2c3
Wasseem Khayrattee <w@7php.com>
2021-11-21 06:48:18 +0400

    feat: add reusable method to check ifNull or empty

--- src/Core/Utils.php
+++ src/Core/Utils.php
@@ -39,4 +39,58 @@ class Utils
         }
         unset($timber);
     }
+
+    /**
+     * Checks whether the value is not empty or not null
+     *
+     * @param $value
+     *
+     * @return bool
+     */
+    public static function notEmptyOrNull($value)
+    {
+        if (is_object($value) && !is_null($value)) {
+            return true;
+        }
+        if (is_array($value)) {
+            if (count($value) == 1) { //to cope with [''] and [' '] arrays
+                if (self::isAssociative($value)) {
+                    return true;
+                } elseif (isset($value[0]) && self::notEmptyOrNull($value[0])) {
+                    return true;
+                }
+
+                return false;
+            }
+            if (sizeof($value) > 0) {
+                return true;
+            }
+
+            return true;
+        } else {
+            if ((is_string($value) || is_int($value)) && ($value != '') && ($value != 'NULL') && (mb_strlen(trim($value)) > 0)) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    /**
+     * To verify if an array is associative
+     *
+     * @param $thatArray
+     *
+     * @return bool
+     */
+    public static function isAssociative($thatArray)
+    {
+        foreach ($thatArray as $key => $value) {
+            if ($key !== (int) $key) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
